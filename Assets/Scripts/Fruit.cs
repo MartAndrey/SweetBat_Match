82022002af5a4c6317a6b96ffab204abce940c8c
Andrey Martin <jeffersonandreyma12@gmail.com>
2022-12-08 19:02:56 -0500

    Add fruits script

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    // Variables that refer to the "fruits" when is selected or which is the last one that was selected
+    static Color selectedColor = new Color(.5f, .5f, .5f, 1);
+    static Fruit previousSelected = null;
+    bool isSelected = false;
+
+    // Unique fruit identifier
+    public int Id { get; set; }
+
+    // Directions to the sides of the "fruit"
+    Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
