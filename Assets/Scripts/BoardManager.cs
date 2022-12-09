29dd84e8a0014ba8db45538cb0db06f39d5fdb9a
Andrey Martin <jeffersonandreyma12@gmail.com>
2022-12-09 16:29:46 -0500

    Board initialization

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -33,11 +33,31 @@ public class BoardManager : MonoBehaviour
         Vector2 offset = currentFruit.GetComponent<BoxCollider2D>().size;
 
         CreateInitialBoard(offset);
-    } 
+    }
 
     // Create the initial elements or fruits of the board
     void CreateInitialBoard(Vector2 offset)
     {
+        fruits = new GameObject[xSize, ySize]; // Columns and rows of the board
 
+        float startX = this.transform.position.x;
+        float startY = this.transform.position.y;
+
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                GameObject newFruit = Instantiate(currentFruit, new Vector3(
+                                                                startX + (offset.x * x),
+                                                                startY + (offset.y * y),
+                                                                0),
+                                                                currentFruit.transform.rotation);
+
+                // Add name to each fruit where we indicate in which column and row it is located
+                newFruit.name = string.Format("Fruit[{0}] [{1}]", x, y);
+
+                fruits[x, y] = newFruit; // Add fruit to the board
+            }
+        }
     }
 }
