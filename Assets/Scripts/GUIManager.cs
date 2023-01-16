f00e64501eddd20ca030662e6796e392660509cd
Andrey Martin <jeffersonandreyma12@gmail.com>
2023-01-16 15:36:09 -0500

    Count remaining moves and score

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -1,8 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GUIManager : MonoBehaviour
 {
-    
+    public static GUIManager Instance;
+
+    public int MoveCounter { get { return moveCounter; } set { moveCounter = value; movesText.text = moveCounter.ToString(); } }
+    public int Score { get { return score; } set { score = value; scoreText.text = score.ToString(); } }
+
+    int moveCounter;
+    int score;
+
+    [SerializeField] TMP_Text movesText, scoreText;
+
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        score = 0;
+        scoreText.text = score.ToString();
+        
+        moveCounter = 30;
+        movesText.text = moveCounter.ToString();
+    }
 }
